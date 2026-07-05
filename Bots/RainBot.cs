7cd1fa328c6c9af2043ef074b3cb271f5791a29f
Mahmoud Darawsheh <darxx03eh@mail.ru>
2026-07-05 11:24:07 +0300

    Applying observer pattern on bots to get notify when weather data changes

---
+++ Bots/RainBot.cs
@@ -0,0 +1,19 @@
+﻿using WeatherMonitoring.Abstractions;
+using WeatherMonitoring.Configurations;
+using WeatherMonitoring.Models;
+
+namespace WeatherMonitoring.Bots;
+
+public class RainBot(BotConfigurations config) : IWeatherObserver
+{
+    private readonly BotConfigurations _config = config;
+    public void Update(WeatherData weather)
+    {
+        if(!_config.Enabled) return;
+        if(weather.Humidity > _config.Threshold)
+            Console.WriteLine($"""
+                               RainBot activated!
+                               RainBot: {_config.Message}
+                               """);
+    }
+}
---
+++ Bots/SunBot.cs
@@ -0,0 +1,19 @@
+﻿using WeatherMonitoring.Abstractions;
+using WeatherMonitoring.Configurations;
+using WeatherMonitoring.Models;
+
+namespace WeatherMonitoring.Bots;
+
+public class SunBot(BotConfigurations config) : IWeatherObserver
+{
+    private readonly BotConfigurations _config = config;
+    public void Update(WeatherData weather)
+    {
+        if (!_config.Enabled) return;
+        if(weather.Temperature > _config.Threshold)
+            Console.WriteLine($"""
+                               SunBot activated!
+                               SunBot: {_config.Message}
+                               """);
+    }
+}
