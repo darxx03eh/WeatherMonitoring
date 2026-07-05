e86459b8f9461cd0c5ac3beb14cff1f86ed8cb14
Mahmoud Darawsheh <darxx03eh@mail.ru>
2026-07-05 10:04:55 +0300

    Add Bot and Application Configurations

---
+++ Configurations/BotConfigurations.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WeatherMonitoring.Configurations;
+
+public sealed class BotConfigurations
+{
+    public bool Enabled { get; set; }
+    public decimal? TemperatureThreshold { get; set; }
+    public decimal? HumidityThreshold { get; set; }
+    [JsonIgnore]
+    public decimal Threshold => TemperatureThreshold ?? HumidityThreshold ?? 0.0m;
+    public string? Message { get; set; }
+}
