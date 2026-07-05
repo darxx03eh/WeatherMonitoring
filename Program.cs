c295d288c87e9035817e264d9c42fbfb7442dd59
Mahmoud Darawsheh <darxx03eh@mail.ru>
2026-07-05 13:26:57 +0300

    Make main method handle exceptions

--- Program.cs
+++ Program.cs
@@ -16,24 +16,31 @@ class Program
     /// <param name="args">Command-line arguments (unused).</param>
     static void Main(string[] args)
     {
-        var config = ConfigurationsLoader.LoadConfigurations();
-        IWeatherStation weatherStation = new WeatherStation();
-        List<IWeatherObserver> observers = new()
+        try
         {
-            new RainBot(config?.RainBot!),
-            new SnowBot(config?.SnowBot!),
-            new SunBot(config?.SunBot!)
-        };
-        observers.ForEach(weatherStation.Register);
-        WeatherData? weatherData = null;
-        Console.Write($"Enter weather data: ");
-        string? input = Console.ReadLine();
-        ParserFactory.GetParser(input).Switch(parser =>
+            var config = ConfigurationsLoader.LoadConfigurations();
+            IWeatherStation weatherStation = new WeatherStation();
+            List<IWeatherObserver> observers = new()
             {
-                weatherData = parser.Parse(input);
-            },
-            argumentException => Console.Error.WriteLine($"Argument Error: {argumentException.Message}"),
-            notSupportedException => Console.Error.Write($"Not Supported Error: {notSupportedException.Message}"));
-        weatherStation.Publish(weatherData);
+                new RainBot(config?.RainBot!),
+                new SnowBot(config?.SnowBot!),
+                new SunBot(config?.SunBot!)
+            };
+            observers.ForEach(weatherStation.Register);
+            WeatherData? weatherData = null;
+            Console.Write($"Enter weather data: ");
+            string? input = Console.ReadLine();
+            ParserFactory.GetParser(input).Switch(parser =>
+                {
+                    weatherData = parser.Parse(input);
+                },
+                argumentException => Console.Error.WriteLine($"Argument Error: {argumentException.Message}"),
+                notSupportedException => Console.Error.Write($"Not Supported Error: {notSupportedException.Message}"));
+            weatherStation.Publish(weatherData);
+        }
+        catch (Exception exp)
+        {
+            Console.Error.WriteLine($"Exception: {exp.Message}");
+        }
     }
 }
