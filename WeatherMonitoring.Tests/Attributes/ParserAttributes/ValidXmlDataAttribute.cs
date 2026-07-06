27600bf3afc276de759845eedf8a599ed9d45f80
Mahmoud Darawsheh <darxx03eh@mail.ru>
2026-07-06 16:55:46 +0300

    Add unit tests from XmlWeatherParser

---
+++ WeatherMonitoring.Tests/Attributes/ParserAttributes/ValidXmlDataAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Reflection;
+using CsvHelper;
+using WeatherMonitoring.Tests.TestModels.Parsers;
+using Xunit.Sdk;
+
+namespace WeatherMonitoring.Tests.Attributes.ParserAttributes;
+
+public class ValidXmlDataAttribute : DataAttribute
+{
+    public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+    {
+        var path = Path.Combine(AppContext.BaseDirectory,
+            "TestData",
+            "Parsers", "XmlWeatherParser",
+            "valid-xml-test-data.csv");
+
+        using var reader = new StreamReader(path);
+        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+        var records = csv.GetRecords<WeatherDataTestCaseRow>();
+
+        foreach (var record in records)
+            yield return [record];
+    }
+}
